633c671d070439edab6c2dfff46e3b3e4bbbb8cd
Soomini96 <91924105+Soomini96@users.noreply.github.com>
2022-01-02 04:33:15 +0900

    Initial commit

---
+++ src/main/java/com/example/restaurant/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.example.restaurant.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String index() {
+        return "/WEB-INF/views/index.jsp";
+    }
+
+}
